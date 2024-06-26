0da22017c8abcc002bec3b7caa4b43089e4e219d
Kevin W Li <kevin.wenji.li@gmail.com>
2024-06-25 23:04:56 -0400

    fixed scroll state upon routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { useLayoutEffect } from "react";
 
+import ScrollReset from "./components/ScrollReset";
 import Home from "./components/Pages/Home";
 import Projects from "./components/Pages/Projects";
+
+
 
 const App: React.FC = () => {
   return (
     // <Router basename="/dda-website">
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <ScrollReset />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
     </Router>
   );
 };

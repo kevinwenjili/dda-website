32c58be14c623c228de46f91f5ce2a053db4fc5a
Kevin W Li <kevin.wenji.li@gmail.com>
2024-06-25 23:07:22 -0400

    fixed scroll state upon routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import { useLayoutEffect } from "react";
 
 import ScrollReset from "./components/ScrollReset";
 import Home from "./components/Pages/Home";
 import Projects from "./components/Pages/Projects";
-
-
 
 const App: React.FC = () => {
   return (
